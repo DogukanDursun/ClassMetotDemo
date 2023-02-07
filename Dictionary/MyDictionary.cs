0b1f4b98dcb753107134183b0178d78a96144703
DogukanDursun <dod63y@gmail.com>
2023-02-07 23:42:15 +0300

    Dictionary

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictionary<T>
+    {
+        T[] Array;
+        T[] _TempArray;
+        public MyDictionary()
+        {
+            Array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _TempArray = Array;
+            Array = new T[_TempArray.Length + 1];
+            for (int i = 0; i < _TempArray.Length; i++)
+            {
+                _TempArray[i] = Array[i];
+            }
+            Array[Array.Length - 1] = item;
+
+
+        }
+        public int Length
+        {
+            get { return Array.Length; }
+        }
+        public void List()
+        {
+            foreach (var ckey in Array)
+            {
+                Console.WriteLine($"Key: {ckey}");
+            }
+            foreach (var cvalue in _TempArray)
+            {
+                Console.WriteLine($"Value: {cvalue}");
+            }
+        }
+    }
+}
+
---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary< string> urunler = new MyDictionary<string>();
+            urunler.Add("Masa");
+            urunler.Add("Sandalye");
+            urunler.Add("Kitap");
+            urunler.Add("Bilgisayar");
+            Console.WriteLine(urunler.Length);
+            Console.ReadLine();
+
+        }
+    }
+}
